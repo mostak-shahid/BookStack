98d385a9ce7a8252147f04921fcec4438d73cb82
mostak-shahid <mostak@primetechbd.com>
2025-07-03 16:31:51 +0600

    Change to 24-12

--- app/Config/database.php
+++ app/Config/database.php
@@ -76,9 +76,15 @@ return [
             'prefix_indexes' => true,
             'strict'         => false,
             'engine'         => null,
-            'options'        => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            // 'options'        => extension_loaded('pdo_mysql') ? array_filter([
+            //     PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            // ]) : [],
+            'options' => extension_loaded('pdo_mysql')
+                ? array_filter([
+                    PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_CA'),
+                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                ])
+                : [],
         ],
 
         'mysql_testing' => [
